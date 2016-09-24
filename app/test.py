b167954d34d8a03957946eee96721b855aa68f2c
Nisarga Patel <np8@umbc.edu>
2016-09-23 23:55:46 -0400

    First commit

---
+++ app/test.py
@@ -0,0 +1,49 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import json
+import requests
+
+#Variables that contains the user credentials to access Twitter API 
+
+count = 0
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+
+        global count
+        data = json.loads(data)
+
+        # curl -d "text=don't feel bad" http://text-processing.com/api/sentiment/
+
+        # old stuff: result = requests.post("http://text-processing.com/api/sentiment/", data= { "text" : data["text"]})
+
+        # old stuff: print "NEGATIVITY: " + str(result.json()["probability"]["neg"]) + data["text"]
+
+
+        count += 1
+
+        if count > 20:
+            return False
+
+        # merge stuff
+
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=['sad', 'angry', 'depressed', 'hate', 'sucks', ':('])
+
