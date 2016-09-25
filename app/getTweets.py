2f30c5a84d921c74b4ec0f649f71dac69b19e4dc
Sumanth Neerumalla <sumanth42gold@gmail.com>
2016-09-25 06:41:58 +0000

    inserting sad tweets into db

--- app/getTweets.py
+++ app/getTweets.py
@@ -5,6 +5,7 @@ import json
 import requests
 import keys
 from toneAnalyzer import analyze_tone
+from pymongo import MongoClient
 
 #Variables that contains the user credentials to access Twitter API 
 
@@ -17,26 +18,29 @@ class StdOutListener(StreamListener):
         
         global count
         data = json.loads(data)
-        
-        tone = analyze_tone(data["text"])
-        id_str = data["id_str"]
-        name = data["user"]["name"]
-        screen_name = data["user"]["screen_name"]
+        try:        
+            tone = analyze_tone(data["text"])
+            id_str = data["id_str"]
+#            name = data["user"]["name"]
+            screen_name = data["user"]["screen_name"]
+            creation_time = data['created_at']
+        except:
+            print (data)
+
+
+
+ #       tones = tone['tones']
 
 
 
         if(tone):
             tone_name = tone["tone_name"]
             if(tone_name == "Sadness"):
+                insertIntoDB(tone,id_str,creation_time);
 
-                print (id_str)
-                print("-----------------------------------")
-                print(tone['score'])
-                print(data["text"])
-                print("------------------------------------")
         count += 1
 
-        if count > 40:
+        if count > 200:
             return False
 
         # merge stuff
@@ -45,6 +49,19 @@ class StdOutListener(StreamListener):
 
     def on_error(self, status):
         print status
+
+
+def insertIntoDB(tone,id_str,creation_time):
+    client = MongoClient()
+    db = client["medHacks"]
+    collection = db["sadTweets"]
+
+    collection.insert({ "tone":tone["score"],
+                          "id_str":id_str,
+                          "creation_time":creation_time
+                      }
+                     )                          
+
 
 
 if __name__ == '__main__':
@@ -56,5 +73,5 @@ if __name__ == '__main__':
     stream = Stream(auth, l)
 
     #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=['sad', 'angry', 'depressed', 'hate', 'sucks', ':('])
+    stream.filter(track=['sad', 'angry', 'depressed', 'hate', 'sucks', ':(','depression','sick','crying','sorry','hurts','hurt'])
 
